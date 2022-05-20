d54798ce5bc6bcd4fdac42746999527d9402fb8d
José María Sosa Resendiz <jose.skedar@gmail.com>
2022-05-20 14:32:05 -0500

    Removing index

--- meta-vote-contract/src/voter.rs
+++ meta-vote-contract/src/voter.rs
@@ -7,10 +7,6 @@ pub struct Voter {
     pub locking_positions: Vector<LockingPosition>,
     pub voting_power: VotePower,
     pub vote_positions: Vector<VotePosition>,
-
-    /// Accumulative count of the locking and vote positions.
-    pub locking_counter: u32,
-    pub vote_counter: u32,
 }
 
 impl Voter {
@@ -20,8 +16,6 @@ impl Voter {
             locking_positions: Vector::new(Keys::LockingPosition.as_prefix(&id).as_bytes()),
             voting_power: 0,
             vote_positions: Vector::new(Keys::VotePosition.as_prefix(&id).as_bytes()),
-            locking_counter: 0,
-            vote_counter: 0,
         }
     }
 }
