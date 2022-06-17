e26ede90d8236b56f12a118d66564ff26d5845d4
saguero <sergiogaguero@gmail.com>
2022-06-17 12:18:18 -0300

    restore data

--- front/lib/pages/components/MyDashboard/DashHeader.tsx
+++ front/lib/pages/components/MyDashboard/DashHeader.tsx
@@ -4,7 +4,6 @@ import {
   Container, 
   Flex, 
   Heading, 
-  LinkOverlay, 
   Text, 
   useDisclosure, 
   Modal,
@@ -22,15 +21,19 @@ import {
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
-  Divider,
-  Spacer,
   VStack,
-  StackDivider
+  StackDivider,
+  toast
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { colors } from '../../../constants/colors';
-import { getAvailableVotingPower } from '../../../lib/near';
+import { getAvailableVotingPower, getBalanceMetaVote, getInUseVotingPower, getLockedBalance, getUnlockingBalance } from '../../../lib/near';
 import { useStore as useWallet } from "../../../stores/wallet";
+import { useStore as useVoter } from "../../../stores/voter";
+import { useFormik } from 'formik';
+import lockValidation from '../../../validation/lockValidation';
+import { yton } from '../../../lib/util';
+import LockModal from './LockModal';
 
 type Props = {
   shortVersion?: boolean
@@ -39,17 +42,25 @@ type Props = {
 const DashboardHeader = (props: Props) => {
   const { wallet, isLogin} = useWallet();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [ sliderValue, setSliderValue] = useState(15);
-  const [ votingPower, setVotingPower] = useState(0);
-  const [ vPowerInUse, setVPowerInUse] = useState(0);
+  const { voterData, setVoterData } = useVoter();
 
+  const initMyData = async ()=> {
+    const newVoterData = voterData;
+    newVoterData.votingPower = await getAvailableVotingPower(wallet);
+    newVoterData.inUseVPower = await getInUseVotingPower(wallet);
+    newVoterData.metaLocked = await getLockedBalance(wallet);
+    newVoterData.metaUnlocking = await getUnlockingBalance(wallet);
+    newVoterData.projectsVoted = await getBalanceMetaVote(wallet); 
+    setVoterData(newVoterData);
+  }
 
   useEffect(  () =>{
     (async ()=> {
-      const vPower = await getAvailableVotingPower();
-      setVotingPower(vPower);
+      if (isLogin && wallet) {
+        initMyData()
+      }
     })();
-  },[])
+  },[wallet, isLogin])
 
 
   return (
@@ -64,78 +75,44 @@ const DashboardHeader = (props: Props) => {
         <Flex mt={20} wrap={'wrap'} justifyContent={{ base: 'center', md: 'space-between' }} flexDirection={{ base: 'column', md: 'row' }}>
           <Box>
             <Text fontSize={'2xl'}>My Voting Power</Text>
-            <Text fontSize={'6xl'} color={colors.primary}>{votingPower}</Text>
+            <Text fontSize={'6xl'} color={colors.primary}>{yton(voterData.votingPower)}</Text>
           </Box>
           <Box>
             <Text fontSize={'2xl'}>In use</Text>
-            <Text fontSize={'6xl'} color={colors.primary}>{vPowerInUse}</Text>
+            <Text fontSize={'6xl'} color={colors.primary}>{voterData.inUseVPower}</Text>
           </Box>
           <Box p={10} border='2px' borderColor={colors.primary} >
             <Text fontSize={'xl'}>Projects Finished</Text>
-            <Text fontSize={'4xl'}>0</Text>
+            <Text fontSize={'4xl'}>{voterData.projectsFinished}</Text>
           </Box>
           <Box p={10} border='2px' borderColor={colors.primary}>
             <Text fontSize={'xl'}>Projects you voted</Text>
-            <Text fontSize={'4xl'}>0</Text>
+            <Text fontSize={'4xl'}>{voterData.projectsVoted}</Text>
           </Box>
         </Flex>
-        <Flex mt={20} wrap={'wrap'} justifyContent={{ base: 'center', md: 'space-between' }} flexDirection={{ base: 'column', md: 'row' }}>
+        <Flex mt={10} wrap={'wrap'} justifyContent={{ base: 'center', md: 'space-between' }} flexDirection={{ base: 'column', md: 'row' }}>
           <Box>
             <Text fontSize={'2xl'}>$META Locked</Text>
-            <Text fontSize={'6xl'} color={colors.primary}>0</Text>
+            <Text fontSize={'6xl'} color={colors.primary}>{yton(voterData.metaLocked)}</Text>
           </Box>
           <Box>
             <Text fontSize={'2xl'}>$META Unlocking</Text>
-            <Text fontSize={'6xl'} color={colors.primary}>0</Text>
+            <Text fontSize={'6xl'} color={colors.primary}>{yton(voterData.metaUnlocking)}</Text>
           </Box>
           <Box>
             <Text fontSize={'xl'}>$META to Withdraw</Text>
-            <Text fontSize={'4xl'}>0</Text>
-            <Button  w={300} colorScheme={colors.primary}>
+            <Text fontSize={'4xl'}>{yton(voterData.metaToWithdraw)}</Text>
+            <Button  w={300} onClick={()=> initMyData()} colorScheme={colors.primary}>
               Withdraw
             </Button>
           </Box>
         </Flex>
       </Container>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>New Lock Position</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <VStack spacing={4} align={'flex-start'}>
-              <Text fontSize={'xs'}>$META Amount {sliderValue}</Text>
-              <InputGroup colorScheme={colors.primary} size='sm'>
-                <InputLeftAddon> $META</InputLeftAddon>
-                <Input colorScheme={colors.primary} placeholder='0' />
-                <InputRightAddon>
-                  <Button h='1.75rem' size='sm'>
-                    Max
-                  </Button>
-                </InputRightAddon>  
-              </InputGroup>
-              <StackDivider></StackDivider >
-              <Slider defaultValue={15} min={0} max={120} step={15} onChange={(val) => setSliderValue(val)}>
-                <SliderTrack bg={colors.primary + '.200'}>
-                  <Box position='relative' right={10} />
-                  <SliderFilledTrack bg={colors.primary} />
-                </SliderTrack>
-                <SliderThumb bg={'gray.500'} boxSize={6} />
-              </Slider>
-              <Text>Autolock Days: {sliderValue}</Text>
-            </VStack>
-          </ModalBody>
-          <ModalFooter>
-            <Button variant='ghost' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme={colors.primary} >Confirm</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <LockModal vPower={voterData.votingPower} isOpen={isOpen} onClose={onClose} wallet={wallet}></LockModal>
 
     </section>
   );
 };
 
 export default DashboardHeader;
+ 
